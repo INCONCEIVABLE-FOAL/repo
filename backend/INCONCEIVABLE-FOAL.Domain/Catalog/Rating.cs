7ec247e5f62e45c2b45fed88ece1e316dec186ff
Ellie Chung <xxellie0602@gmail.com>
2021-04-09 21:43:53 -0400

    finished lab 4

---
+++ backend/INCONCEIVABLE-FOAL.Domain/Catalog/Rating.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace INCONCEIVABLE.FOAL.Domain.Catalog
+{
+    public class Rating
+    {
+        public int Star { get; set; }
+        public string UserName { get; set; }
+        public string Review { get; set; }
+
+        public Rating(int stars, string UserName, string Review)
+        {
+            if (stars < 1 || stars > 5)
+            {
+                throw new ArgumentException("Star rating must be an integer: 1, 2, 3, 4, or 5.");
+            }
+
+            if (string.IsNullOrEmpty(UserName))
+            {
+                throw new ArgumentException("UserName cannot be null.");
+            }
+
+            this.Star = stars;
+            this.UserName = UserName;
+            this.Review = Review;
+        }
+    }
+}
+
