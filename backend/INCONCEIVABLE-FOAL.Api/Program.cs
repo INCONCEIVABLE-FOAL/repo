ebdef37dc0e0ced426188691bc66f5ae6ce15361
Madison Shimborske <shimborske.1@osu.edu>
2021-04-15 11:11:02 -0700

    lab 4 part 4

--- backend/INCONCEIVABLE-FOAL.Api/Program.cs
+++ backend/INCONCEIVABLE-FOAL.Api/Program.cs
@@ -13,7 +13,9 @@ namespace INCONCEIVABLE_FOAL.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +24,25 @@ namespace INCONCEIVABLE_FOAL.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    context.Database.EnsureCreated();
+                    DbInitializer.Initialize(context, logger);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error occurred creating the database.");
+                }
+            }
+        }
     }
 }
